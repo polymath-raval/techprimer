8596a28d87d8444a82d801bffdb899686923e978
Nikunj Raval <polymath.raval@gmail.com>
2017-12-10 22:46:47 +0530

    Adding Eureka for db service and stock service

--- springboot/stockviewer/db-service/src/main/java/com/raval/polymath/techprimers/springboot/stockviewer/dbservice/DbServiceApplication.java
+++ springboot/stockviewer/db-service/src/main/java/com/raval/polymath/techprimers/springboot/stockviewer/dbservice/DbServiceApplication.java
@@ -2,10 +2,12 @@ package com.raval.polymath.techprimers.springboot.stockviewer.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "com.raval.polymath.techprimers.springboot.stockviewer.dbservice.repository")
 @SpringBootApplication
+@EnableEurekaClient
 public class DbServiceApplication {
 
   public static void main(String[] args) {
--- springboot/stockviewer/stock-service/src/main/java/com/raval/polymath/techprimers/springboot/stockviewer/stockservice/config/StockConfig.java
+++ springboot/stockviewer/stock-service/src/main/java/com/raval/polymath/techprimers/springboot/stockviewer/stockservice/config/StockConfig.java
@@ -1,16 +1,17 @@
 package com.raval.polymath.techprimers.springboot.stockviewer.stockservice.config;
 
-//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-//@EnableEurekaClient
+@EnableEurekaClient
 public class StockConfig {
 
   @Bean
+  //@LoadBalanced
   public RestTemplate getRestTemplate(){
     return new RestTemplate();
   }
